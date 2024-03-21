a80d093d57b81b78cae9b25e93e3fa1152fe0503
LP <lsingh@questrade.com>
2024-03-21 14:03:34 -0400

    added comments

--- angular-mfe-remote/custom-webpack.config.js
+++ angular-mfe-remote/custom-webpack.config.js
@@ -2,20 +2,21 @@ const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
   output: {
-    uniqueName: "angular-mfe-remote",
-    publicPath: "auto",
+    uniqueName: "angular-mfe-remote", // unique name for the remote app
+    publicPath: "auto", // From which path or URL to serve dist files, find automatically
   },
   optimization: {
-    runtimeChunk: false,
+    runtimeChunk: false, // Whether to create a separate file for the webpack runtime
   },
   experiments: {
-    outputModule: true,
+    outputModule: true, // We want to export files as ES Modules
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "angular-mfe-remote",
-      filename: "remoteEntry.js",
+      name: "angular-mfe-remote", // Unique name for this remote app
+      filename: "remoteEntry.js", // What would be the name for the remote entry file
       exposes: {
+        // What to expose
         "./Customer": "./src/app/customer/customer.component.ts",
       },
       remotes: [],
@@ -37,7 +38,7 @@ module.exports = {
         },
       },
       library: {
-        type: "module",
+        type: "module", // Expose as ES Modules
       },
     }),
   ],
